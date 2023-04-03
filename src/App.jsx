dc292101f59172184a37ca4e8a81e93257ce1ec7
fedorronginskij <fedorronginskij@gmail.com>
2023-04-03 16:51:30 +0300

    Добавление новых комментариев

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css'
 import { useDispatch, useSelector } from 'react-redux';
+// Импортируем созданные функции для клиентов
 import { addCustomerAction, removeCustomerAction } from './store/customerReducer';
 import { fetchCustomers } from './asyncActions/customers';
 
@@ -34,16 +35,21 @@ function App() {
         // Проделываем те же самые операции, что и в функции addCash
         dispatch({type:"GET_CASH", payload: cash})
     }
-
+    // Создаём стрелочную функцию для добавления нового клиента, где параметром принимается имя клиента
     const addCustomer = (name) => {
+        // Данные - это объект клиента
         const customer = {
+            // Передаём имя параметром
             name,
+            // Присваииваем id, который получаем из текущего времени
             id: Date.now(),
         }
+        // Внутри функции dispatch вызываем функцию addCustomerAction, куда передаём данные
         dispatch(addCustomerAction(customer))
     }
-
+    // Создаём стрелочную функцию, где в качестве параметра передаём клиента
     const removeCustomer = (customer) => {
+        // Внутри функции dispatch вызываем функцию removeCustomerAction, где в качестве параметра передаём id клиента
         dispatch(removeCustomerAction(customer.id))
     }
 
@@ -54,6 +60,12 @@ function App() {
          * Вешаем на кнопки слушатель событий onClick и вызываем необходимые функции.
          * Параметром передаём функцию prompt, которая позволяет открыть окно с полем ввода в браузере.
          * Поскольку функция возвращает строку, её необходимо с помощью Number преобразовать к числовому значению.
+         * Далее создаём условие, в котором будем проверять пустоту массива customers.
+         * Если массив не пустой, то будем его отрисовывать.
+         * В обратном случае, если массив пустой, то выводим сообшение об отсутствии клиентов.
+         * С помощью функции map итерируемся по массиву customers.
+         * Для каждого customers создаём блок div, в котором разворачиваем объект клиента и указываем его имя.
+         * Вешаем слушатель событий на каждого клиента, где при нажатии на клиента будет происходить его удаление из списка.
          */
         <div className={'app'}>
         <div style={{fontSize:"3rem"}}>{cash}</div>
--- src/store/customerReducer.js
+++ src/store/customerReducer.js
@@ -6,7 +6,7 @@
 const defaultState = {
     customers: []
 }
-
+// Выносим названия actions в отдельные константы
 const ADD_CUSTOMER = "ADD_CUSTOMER"
 const ADD_MANY_CUSTOMERS = "ADD_MANY_CUSTOMERS"
 const REMOVE_CUSTOMERS = "REMOVE_CUSTOMERS"
@@ -19,18 +19,36 @@ export const customerReducer = (state = defaultState, action) => {
         /* 
          * Меняем названия типа action.
          * Создаём actions для добавления одного или нескольких покупателей в список, а также для удаления покупателей.
+         * В кейсах используем ранее созданные константы.
          */
       case ADD_MANY_CUSTOMERS:
+        /*
+         * Возвращаем новый объект, в который разворачиваем старое состояние.
+         * Поскольку это операция добавление нового клиента, то присваиваем customers новый массив.
+         * В новый массив разворачиваем уже существующий массив customers.
+         * В конец добавляем объект, который будем передавать через action.
+         */
            return {...state, customers: [...state.customers, ...action.payload]}
       case ADD_CUSTOMER:
           return {...state, customers: [...state.customers, action.payload]}
       case REMOVE_CUSTOMERS: 
+        /*
+         * Проделываем почти ту же последовательность действий, что и в случае с ADD_CUSTOMER.
+         * Разница между кейсами будет заключаться в том, что здесь мы фильтруем массив customers.
+         * Используем функцию filter, которая возвращает новый массив.
+         * В новый массив попадают только те объекты, для которых функция-колбэк вернула true.
+         * Если id клиента равняется тому id, который будем передавать как payload, то элемент не попадёт в новый массив.
+         */
           return {...state, customers: state.customers.filter(customer => customer.id !== action.payload)}
       default:
         return state
     }
   }
-  
+  /*
+   * Создаём простейшие функции, которые будут возвращать объекты.
+   * Параметром функция будет принимать какие-то данные.
+   * Функция вернёт объект с типом action и какими-то данными, которые передадим в параметры.
+   */
   export const addCustomerAction = (payload) => ({type: ADD_CUSTOMER, payload})
   export const addManyCustomerAction = (payload) => ({type: ADD_MANY_CUSTOMERS, payload})
   export const removeCustomerAction = (payload) => ({type: REMOVE_CUSTOMERS, payload})
